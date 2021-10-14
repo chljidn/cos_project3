20bf0b555166c52d9f957173a7e1c7a7bd984c57
chljidn <chljidn@nate.com>
2021-10-14 10:48:36 +0900

    20211014

--- cos/settings.py
+++ cos/settings.py
@@ -190,5 +190,5 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-
+    ]
 }
