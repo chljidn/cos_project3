b9f6932a024bc52fbd0f7960e141350291025a50
chljidn <chljidn@nate.com>
2021-10-16 00:37:08 +0900

    20211016

--- common/views/auth_views.py
+++ common/views/auth_views.py
@@ -4,7 +4,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+# 각 요청에 대한 허융/거부
+from rest_framework.permissions import IsAuthenticated
+
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 from common.serializers import MyTokenObtainPairSerializer
 
 # rest_framework의 authtoken을 이용한 로그인. 토큰 발급
@@ -22,13 +26,31 @@ class login(ObtainAuthToken):
                             })
         else:
             return Response({'message': '아이디를 다시 입력하세요'})
-#-----------------------------------------------
+
 class logout(APIView):
     def get(self, request, format=None):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
 
+# -----------------------------------------------------------------
+
+# login
 # simplejwt을 이용한 Token 발급
 # access, refresh Token 발급
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+# logout
+# 형식은 refresh token을 받아서 blacklist에 추가시키는 것. refresh token은 content 부분에 담겨 보내진다.
+# 코드 출처 : https://medium.com/django-rest/logout-django-rest-framework-eb1b53ac6d35
+class logout(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, reuqest):
+        try:
+            refresh_token = reuqest.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
